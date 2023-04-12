dd6d3c2233af89795fcddafaa66d5abce39769d7
SHKBRD <121646151+SHKBRD@users.noreply.github.com>
2023-04-12 10:40:49 -0400

    added template to main.py

--- main.py
+++ main.py
@@ -4,3 +4,13 @@ pygame.init()
 pygame.font.init()
 
 SCREEN_WIDTH, SCREEN_HEIGHT = 800, 800
+
+run = True
+while run:
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+            
+    
+    pygame.display.update()
