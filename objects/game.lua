4816989c286a096bfefec9360589019ee2c3c3fb
SHKBRD <121646151+SHKBRD@users.noreply.github.com>
2023-04-13 00:37:44 -0400

    bell sound highlight current cd drive selection redraw all cds

--- objects/game.lua
+++ objects/game.lua
@@ -1,4 +1,5 @@
 local CD = require("objects/cd")
+local Cursor = require("objects/cursor")
 local Game = {}
 
 Game.cds = {}
@@ -12,9 +13,29 @@ for x = 0, 4 do
 end
 
 local bellSprite = love.graphics.newImage("assets/sprites/bell.png")
+local bellSound = love.audio.newSource("assets/sounds/bell.mp3", "stream")
 
-function Game:mousepressed (x, y)
-	
+local Bell = {
+	x = 10,
+	y = shelfSize - 10,
+	width = bellSprite:getWidth(),
+	height = bellSprite:getHeight()
+}
+
+function Game:mousepressed (mx, my, button)
+	if button == 1 then
+		print("Click!")
+	end
+
+	--Over any cd
+	if button == 2 then
+		print("Right!")
+	end
+
+	--Bell clicked
+	if mx/2 > Bell.x and mx/2 < Bell.x + Bell.width and my/2 > Bell.y and my/2 < Bell.y + Bell.height then
+		bellSound:play()
+	end
 end
 
 function Game:draw ()
@@ -23,7 +44,7 @@ function Game:draw ()
 		love.graphics.rectangle("fill", slot[1], slot[2], slot[3], slot[4])
 		love.graphics.setColor(24 / 255, 24 / 255, 37 / 255)
 		love.graphics.rectangle("fill", slot[1] + 1, slot[2] + 1, slot[3] - 2, slot[4] - 2)
-		if slot[4] then
+		if slot[4] and not ((Cursor.x > slot[1]) and (Cursor.x < slot[1] + slot[3]) and (Cursor.y > slot[2]) and (Cursor.y < slot[2] + slot[4])) then
 			love.graphics.setColor(205 / 255, 214 / 255, 244 / 255)
 			love.graphics.rectangle("fill", slot[1] + 2, slot[2] + 2, slot[3] - 4, slot[4] - 4)
 		end
@@ -37,7 +58,7 @@ function Game:draw ()
 		cd:draw()
 	end
 	love.graphics.setColor(1, 1, 1)
-	love.graphics.draw(bellSprite, 10, shelfSize - 10)
+	love.graphics.draw(bellSprite, Bell.x, Bell.y)
 end
 
 return Game
